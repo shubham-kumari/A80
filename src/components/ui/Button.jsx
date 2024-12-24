d6440b9f17c184bee7e823127628d75084de3842
shubham kumari <kumarishubham273@gmail.com>
2024-12-25 01:11:51 +0530

    25 dec

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,14 +1,19 @@
 import side from "../../assets/G.svg";
-import arrow from '../../assets/arrow-right.svg';
+import arrow from "../../assets/arrow-right.svg";
 
 export default function Button({ children }) {
   return (
     <div className="flex items-center ">
-      <img src={side} alt="Side Image"  />
-      <div className="flex items-center gap-2 bg-white px-4 py-3">
+      <img src={side} alt="Side Image" />
+      <div className="group flex items-center gap-2 bg-white px-4 py-3 relative overflow-hidden">
         <button className="uppercase font-semibold">{children}</button>
-        <img src={arrow} alt="Arrow" className="w-4 h-4" />
+        <img
+          src={arrow}
+          alt="Arrow"
+          className="w-4 h-4 transform translate-x-0 transition-transform duration-300 group-hover:translate-x-2"
+        />
       </div>
+
       <img src={side} alt="Side Image" className="scale-x-[-1]" />
     </div>
   );
