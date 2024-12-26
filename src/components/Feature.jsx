a824a86eb3371a53f9d527c0d94dfd486431a283
shubham kumari <kumarishubham273@gmail.com>
2024-12-26 23:26:08 +0530

    26 dec 2

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,45 +1,46 @@
-import screen from "../assets/screen.svg";
+import React, { useState } from "react";
+import mm from "../assets/8.54mm.svg";
 import ip54 from "../assets/IP54.svg";
-import splash from "../assets/splash.svg";
+import dust from "../assets/dust.svg";
 import waterResistant from "../assets/waterResistant.svg";
 
 export default function Feature() {
+  const [step, setStep] = useState(0);
+  const images = [
+    { src: ip54, alt: "IP54 rating" },
+    { src: dust, alt: "Dust resistant" },
+    { src: waterResistant, alt: "Water Resistant" },
+    { src: mm, alt: "8.54mm screen" },
+  ];
+
+  const handleClick = () => {
+    setStep((prevStep) => (prevStep + 1) % images.length);
+  };
+
   return (
-    <>
-      <div className="flex flex-col px-32 items-center text-[#161410]">
-        <div className="flex text-[#161410] pt-32">
-          <p className="font-markot font-medium text-5xl">
-            Built for Life’s Unexpected Moments
-          </p>
-          <p className="font-markot text-base text-grey/grey/3">
-            A80's built to handle the challenges of an active lifestyle, whether
-            you’re caught in a rain or out in a dusty environment.
-          </p>
-        </div>
-        <div className="flex flex-col items-center">
-          <img
-            src={screen}
-            alt="A phone that has screen on"
-            className="relative"
-          />
-          <img
-            src={ip54}
-            alt="A phone that has screen on"
-            className=""
-          />
-          <div className="flex items-center">
-            <div className="flex gap-1">
-              <img src={waterResistant} alt="water Resistant icon" />
-              <p className="font-markot font-medium text-xl">Splash Resistant</p>
-            </div>
-            <img
-              src={splash}
-              alt="A water splash"
-              className=" "
-            />
-          </div>
-        </div>
+    <div className="flex flex-col px-32 items-center text-[#161410]">
+      <div className="flex text-[#161410] pt-32">
+        <p className="font-markot font-medium text-5xl">
+          Built for Life’s Unexpected Moments
+        </p>
+        <p className="font-markot text-base text-grey/grey/3">
+          A80's built to handle the challenges of an active lifestyle, whether
+          you’re caught in a rain or out in a dusty environment.
+        </p>
       </div>
-    </>
+      <div className="relative flex flex-col items-center" onClick={handleClick}>
+        
+        {images.map((image, index) => (
+          <img
+            key={index}
+            src={image.src}
+            alt={image.alt}
+            className={` transition-all duration-500 ease-in-out ${
+              step === index ? "opacity-100 scale-100 z-1000" : "opacity-0 scale-50 z-0"
+            }`}
+          />
+        ))}
+      </div>
+    </div>
   );
 }
